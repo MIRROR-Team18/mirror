154ad398ba031d9ac0c00dd1d32c113da6fb1dd6
Pear <pkedzia88@gmail.com>
2023-10-17 15:15:33 +0100

    Amended folder structure, added manage.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MIЯЯOR</title>
-    <link rel="stylesheet" href="stylesheets/main.css">
+    <link rel="stylesheet" href="_stylesheets/main.css">
 </head>
 <body>
-    <?php include 'components/header.php'; ?>
+    <?php include '_components/header.php'; ?>
     <header>
         <h1>MIЯЯOR</h1>
         <p>we'll have a slogan right?</p>
@@ -22,13 +22,13 @@
                 // Example code for fetching products from the database
                 // $products = $db->query('SELECT * FROM products ORDER BY id DESC LIMIT 4');
                 // foreach ($products as $product) {
-                //     include 'components/product.php';
+                //     include '_components/product.php';
                 // }
                 ?>
             </div>
             <button>See the range...</button>
         </section>
     </main>
-    <?php include 'components/footer.php'; ?>
+    <?php include '_components/footer.php'; ?>
 </body>
 </html>
