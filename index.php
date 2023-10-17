69aa82ab2ab0d00024f1bfae2f246a5ece007f51
Pear <pkedzia88@gmail.com>
2023-10-17 12:34:41 +0100

    Started working on a home page based on my design

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MIЯЯOR</title>
+    <link rel="stylesheet" href="stylesheets/main.css">
 </head>
 <body>
+    <?php include 'components/header.php'; ?>
     <h1>HELLO MIЯЯOR</h1>
+    <?php include 'components/footer.php'; ?>
 </body>
 </html>
