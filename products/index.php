84ccc0091b18b14b0bb3d47b595c63b8bb31a71f
Pear <pkedzia88@gmail.com>
2023-11-16 19:09:41 +0000

    Started working on the products listing. products/index.php is accessible on the website under products/

---
+++ products/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Products - MIRЯOR</title>
+	<link rel="stylesheet" href="../_stylesheets/main.css">
+</head>
+<body>
+    <?php include '../_components/header.php'; ?>
+    <header>
+        <h1>PRODUCTS</h1>
+        <p>we have products. see them.</p>
+    </header>
+    <main>
+
+    </main>
+    <?php include '../_components/footer.php'; ?>
+</body>
+</html>
