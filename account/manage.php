03f9e48356ef11766fe6fc60e6528ebecc0ed161
Jack <220098814@aston.ac.uk>
2023-11-17 03:00:21 +0000

    Made it so you can swap between the 3 segments on the management page and also added code which ensures the user is logged into an account before accessing the page

--- account/manage.php
+++ account/manage.php
@@ -1,4 +1,13 @@
-<?php $currentView = "details" ?>
+<?php
+    if (isset($_GET['option']) && in_array($_GET['option'], array('details', 'security', 'pastOrders'))) {
+        $currentView = $_GET['option'];
+    }
+    else {
+        $currentView = "details";
+    }
+    session_start();
+    if (!isset($_SESSION["signedin"])) $_SESSION["signedin"] = false;
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,47 +20,50 @@
     <link rel="stylesheet" href="../_stylesheets/accountManage.css">
 </head>
 <body>
+    <!-- Check if user is logged into an account if not send them back to the home page since they are trying to access this page by entering in the 
+    url manualy without being logged into an account which will potentially cause errors fetching data etc later in the page -->
+    <?php
+        if ($_SESSION["signedin"] == false) echo '<script>window.location.replace("../index.php");</script>';
+    ?>
     <?php include '../_components/header.php'; ?>
     <main>
         <h1>Account Management</h1>
         <section id="accountPanel">
             <div id="options">
-                <a>Your Details</a>
-                <a>Security</a>
-                <a>Past Orders</a>
+                <a href = "manage.php?option=details">Your Details</a><br><br>
+                <a href = "manage.php?option=security">Security</a><br><br>
+                <a href = "manage.php?option=pastOrders">Past Orders</a>
             </div>
             <div id="view">
-                <?php if ($currentView == "details") { // Could this be a switch/case? ?>
-                    <div class="row">
-                        <label for="firstName">First Name</label>
+                <?php
+                switch ($currentView) {
+                    case "details":
+                        echo 
+                        '<label for="firstName">First Name</label>
                         <input type="text" name="firstName" id="firstName" value="">
-
                         <label for="surName">Last Name</label>
-                        <input type="text" name="surName" id="surName" value="">
-                    </div>
-                    <div class="row">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" id="email">
-                    </div>
-                <?php } else if ($currentView == "security") { ?>
-                    <div class="row">
-                        <label for="currentPassword">Current Password</label>
-                        <input type="password" name="currentPassword" id="currentPassword">
-                    </div>
-                    <div class="row">
-                        <label for="newPassword">Current Password</label>
-                        <input type="password" name="newPassword" id="newPassword">
-                    </div>
-                    <div class="row">
-                        <label for="confirmNewPassword">Current Password</label>
-                        <input type="password" name="confirmNewPassword" id="confirmNewPassword">
-                    </div>
-                <?php } else if ($currentView == "orders") {
-                    // Make call to database to return orders.
-                    ?>
-                    <p>#0000000 | Hardtail Shoes | UK 11 | £59.99</p>
-                    <?php
-                } else { throw new LogicException("View matches nothing."); } ?>
+                        <input type="text" name="surName" id="surName" value="">';
+                        break;
+                    case "security":
+                        echo
+                        '<div class="row">
+                            <label for="currentPassword">Current Password</label>
+                            <input type="password" name="currentPassword" id="currentPassword">
+                        </div>
+                        <div class="row">
+                            <label for="newPassword">New Password</label>
+                            <input type="password" name="newPassword" id="newPassword">
+                        </div>
+                        <div class="row">
+                            <label for="confirmNewPassword">Confirm New Password</label>
+                            <input type="password" name="confirmNewPassword" id="confirmNewPassword">
+                        </div>';
+                        break;
+                    case "pastOrders":
+                        // Make call to database to return orders.
+                        break;
+                }
+                ?>
             </div>
         </section>
     </main>
