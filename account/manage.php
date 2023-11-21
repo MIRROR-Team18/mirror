a00bf179d89cf2da289e1dce2f6bde3fce07daac
Jack <220098814@aston.ac.uk>
2023-11-21 15:16:51 +0000

    Continued work on "details" section of the page

--- account/manage.php
+++ account/manage.php
@@ -1,12 +1,11 @@
 <?php
-    if (isset($_GET['option']) && in_array($_GET['option'], array('details', 'security', 'pastOrders'))) {
+    session_start();
+    if (isset($_GET['option']) && in_array($_GET['option'], array('details', "details-change", 'security', 'pastOrders'))) {
         $currentView = $_GET['option'];
     }
     else {
         $currentView = "details";
     }
-    session_start();
-    if (!isset($_SESSION["signedin"])) $_SESSION["signedin"] = false;
 ?>
 <!doctype html>
 <html lang="en">
@@ -23,7 +22,7 @@
     <!-- Check if user is logged into an account if not send them back to the home page since they are trying to access this page by entering in the 
     url manualy without being logged into an account which will potentially cause errors fetching data etc later in the page -->
     <?php
-        if ($_SESSION["signedin"] == false) echo '<script>window.location.replace("../index.php");</script>';
+        //if (isset($_SESSION["userID"]) == false) echo '<script>window.location.replace("../index.php");</script>';
     ?>
     <?php include '../_components/header.php'; ?>
     <main>
@@ -38,15 +37,37 @@
                 <?php
                 switch ($currentView) {
                     case "details":
-                        echo 
-                        '<label for="firstName">First Name</label>
-                        <input type="text" name="firstName" id="firstName" value="">
-                        <label for="surName">Last Name</label>
-                        <input type="text" name="surName" id="surName" value="">';
+                        //Needs to get names and stuff from username based on userID
+                        $fName = "FIRST NAME";
+                        $sName = "LAST NAME";
+                        $email = "EMAIL"; 
+                        echo'
+                        <p>First Name: ' . $fName . ' </p> 
+                        <p>Last Name: ' . $sName . ' </p>
+                        <p>Email: ' . $email . ' </p><br>
+                        <a href = "manage.php?option=details-change"><input class = "button" type = "submit" value = "Change Details"></a>
+                        ';
+                        break;
+                    case "details-change":
+                        //Following stuff needs to be updated to the database when the form is submited
+                        echo '
+                        <form>
+                            <label for="firstName">First Name</label>
+                            <input type="text" name="firstName" id="firstName" value="">
+                            <br>
+                            <label for="surName">Last Name</label>
+                            <input type="text" name="surName" id="surName" value="">
+                            <br>
+                            <label for="email">Email</label>
+                            <input type="text" name = "email" id="email" value="">
+                            <br><br>
+                            <input class = "button" type="submit" value = "Submit Changes">
+                        </form>
+                        ';
                         break;
                     case "security":
-                        echo
-                        '<div class="row">
+                        echo'
+                        <div class="row">
                             <label for="currentPassword">Current Password</label>
                             <input type="password" name="currentPassword" id="currentPassword">
                         </div>
@@ -57,7 +78,8 @@
                         <div class="row">
                             <label for="confirmNewPassword">Confirm New Password</label>
                             <input type="password" name="confirmNewPassword" id="confirmNewPassword">
-                        </div>';
+                        </div>
+                        ';
                         break;
                     case "pastOrders":
                         // Make call to database to return orders.
