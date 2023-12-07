e0aee38e01a7d3a7bff642515c9e9e4e35f6c2e7
Harleen Kaur <220147097@aston.ac.uk>
2023-12-07 19:01:55 +0000

    made checkout and processedCheckout page

--- basket.php
+++ basket.php
@@ -3,48 +3,60 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basket</title>
+    <title>Your Shopping Basket - MIЯЯOR</title>
     <link rel="stylesheet" href="_stylesheets/main.css">
-    <link rel="stylesheet" href="./_stylesheets/basket.css">
+    <link rel="stylesheet" href="_stylesheets/basket.css">
 </head>
 <body>
     <?php include '_components/header.php'; ?>
-    <h1>Current Basket</h1>
-    <table id="basket">
-        <thead>
-            <tr>
-                <th>Product</th>
-                <th>Description</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- Add your product rows dynamically here using JavaScript -->
-            <tr>
-                <td><img src="./_images/peach.jpg" alt="Hoodie" class="product-image"></td>
-                <td>
-                    <p><strong>Hoodie</strong></p>
-                    <p>Color: Peach</p>
-                </td>
-                <td>1</td>
-                <td>£20.00</td>
-                <td><!-- Actions buttons go here --></td>
-            </tr>
-        </tbody>
-    </table>
 
-    <!-- Order Summary Section -->
-    <div id="order-summary">
-        <h2>Order Summary</h2>
-        <p>Order Value: £20.00</p>
-        <!-- Add other order summary details as needed -->
-        <div id="total">TOTAL: £20.00</div>
-        <button id="continue-to-checkout">Continue to Checkout</button>
+    <div class="basket-container">
+        <h1>Your Shopping Basket</h1>
+        <table id="basket">
+            <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Description</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Add your product rows dynamically here using PHP -->
+                <tr>
+                    <td><img src="./_images/peach.jpg" alt="Hoodie" class="product-image"></td>
+                    <td>
+                        <p><strong>Hoodie</strong></p>
+                        <p>Color: Peach</p>
+                    </td>
+                    <td>
+                    <select>
+            <?php
+                // Assuming a maximum quantity of 10, you can adjust as needed
+                for ($i = 1; $i <= 10; $i++) {
+                    echo "<option value='$i'>$i</option>";
+                }
+            ?>
+        </select>
+                    </td>
+                    <td>£19.99</td>
+                    <td><!-- Actions buttons go here --></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <!-- Order Summary Section -->
+        <div id="order-summary">
+            <h2>Order Summary</h2>
+            <p>Order Value: £19.99</p>
+            <!-- Add other order summary details as needed -->
+            <div id="total">TOTAL: £19.99</div>
+            <!-- Use an anchor tag around the button for navigation -->
+            <a href="checkout.php" id="continue-to-checkout">Continue to Checkout</a>
+        </div>
     </div>
 
-    <script src="basket.js"></script>
     <?php include '_components/footer.php'; ?>
 </body>
 </html>
---
+++ processedCheckout.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order Confirmation - MIЯЯOR</title>
+    <link rel="stylesheet" href="_stylesheets/main.css">
+    <link rel="stylesheet" href="./_stylesheets/basket.css">
+</head>
+<body>
+    <?php include '_components/header.php'; ?>
+
+    <div class="confirmation-container">
+        <h1>Your Order Has Been Confirmed!</h1>
+        <p>Orders usually take 3-5 working days to be delivered.</p>
+        <p>To track your order, go to <a href="http://ww*.trackmyMIRROR.com" target="_blank">www.trackmyMIRROR.com</a></p>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br></br> <!--need a proper soution to move footer to the bottom -->
+    <?php include '_components/footer.php'; ?>
+</body>
+</html>
+
+
