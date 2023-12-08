e1b902eef8654f5bd4052c34a8f167741e7b7d45
Pear <pkedzia88@gmail.com>
2023-12-08 13:40:57 +0000

    Added backend for adding products to basket

--- products/product.php
+++ products/product.php
@@ -1,3 +1,16 @@
+<?php
+    require_once '../_components/database.php';
+    $db = new Database();
+
+    if (isset($_POST['product_id'])) {
+        if (session_status() === PHP_SESSION_NONE) session_start();
+        if (!isset($_SESSION['basket'])) $_SESSION['basket'] = array();
+
+        $product = $db->getProduct($_POST['product_id']);
+        $_SESSION['basket'][] = $product;
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,36 +21,37 @@
     <link rel="stylesheet" href="../_stylesheets/product.css">
 </head>
 <body>
-    <?php include '../_components/header.php'; ?>
+<?php include '../_components/header.php'; ?>
 
-    <div class="container">
-        <?php
-           include '../_components/database.php';
-           $db = new Database();
+<div class="container">
+	<?php
+	// This will get product ID from URL
+    if (!isset($_GET['id'])) {
+        header("Location: /products");
+        exit();
+    }
+	$product_id = $_GET['id'];
 
-           // This will get product ID from URL
-           $product_id = isset($_GET['id']) ? $_GET['id'] : 0;
-           
-           // This will fetch product from the database
-           $product = $db->getProduct($product_id);
-           
-           if (!is_null($product)) {
-               echo "<h1>" . $product->name . "</h1>";
-               echo "<p>" . $product->type . "</p>";
-               echo "<p>Price: $2.00</p>";
+	// This will fetch product from the database
+	$product = $db->getProduct($product_id);
 
-               echo "<form action='add_to_cart.php' method='post'>";
-               echo "<input type='hidden' name='product_id' value='" . $product_id . "'>";
-               echo "<input type='submit' value='Add to Cart'>";
-               echo "</form>";
+	if (!is_null($product)) {
+		echo "<h1>" . $product->name . "</h1>";
+		echo "<p>" . $product->type . "</p>";
+		echo "<p>Price: $2.00</p>";
+
+		echo "<form action='' method='post'>";
+		echo "<input type='hidden' name='product_id' value='" . $product_id . "'>";
+		echo "<input type='submit' value='Add to Cart'>";
+		echo "</form>";
 
 
-           } else {
-               echo "Product not found";
-           }
-       ?>
-    </div>
+	} else {
+		echo "Product not found";
+	}
+	?>
+</div>
 
-    <?php include '../_components/footer.php'; ?>
+<?php include '../_components/footer.php'; ?>
 </body>
 </html>
