d82fa988d8c7e7e876de2c2d8aaae7fc71345e2c
DurgaM <220176950@aston.ac.uk>
2024-02-08 15:09:41 +0000

    Rebase

--- login.php
+++ login.php
@@ -29,7 +29,18 @@ if (isset($_POST['submitted'])) {
     }
 }
 
-  
+    //Creating the OTP
+
+    $sql = "SELECT * FROM User WHERE email='$email'";
+    $query = mysqli_query($conn, $sql);
+    $code = mysqli_fetch_array($query);
+
+    if ($code && password_verify($password, $data['password'])) {
+        $otp = rand(100000, 999999);
+        $otp_expiry = date("Y-m-d H:i:s", strtotime("+3 minute"));
+        $subject= "Your OTP for Login";
+        $message="Your OTP is: $otp";
+}
 ?>
 
 
