3e88bd07f75f37e8549d26069d18d5b58e8bfe88
Pear <pkedzia88@gmail.com>
2024-02-17 21:20:14 +0000

    Defined structure for admin

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<?php require_once("./_components/adminCheck.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include '../_components/default.php'; ?>
+	<title>Admin - MIRЯOR</title>
+	<link rel="stylesheet" href="./admin.css">
+</head>
+<body>
+	<?php include './_components/header.php'; ?>
+	<main>
+		<section>
+			<h1>WELCOME BACK</h1>
+			<p></p>
+		</section>
+	</main>
+</body>
+</html>
