f2078480a54084f1be3131b8bdcca1b8c5989eeb
PearOrchards <pkedzia88@gmail.com>
2024-02-19 18:16:29 +0000

    Created /admin/products.php

--- admin/_components/header.php
+++ admin/_components/header.php
@@ -1,10 +1,11 @@
 <?php /* Admin pages will already have session_start activated */ ?>
 <nav>
 	<div class="left">
-		<a href="/admin"><img src="/_images/logo_square.svg" alt=""></a>
+		<a href="/"><img src="/_images/logo_square.svg" alt=""></a>
 	</div>
 	<div class="right">
-		<a href="/"><i class="fa-solid fa-house"></i>Return Home</a>
+		<a href="/admin"><i class="fa-solid fa-house"></i>Home</a>
+        <a href="/admin/products/"><i class="fa-solid fa-cart-shopping"></i>Products</a>
 		<a href="/logout.php"><i class="fa-solid fa-right-from-bracket"></i>Logout</a>
 	</div>
 </nav>
---
+++ admin/products/index.php
@@ -0,0 +1,16 @@
+<?php require_once("../_components/adminCheck.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include '../../_components/default.php'; ?>
+	<title>Products Admin - MIRЯOR</title>
+	<link rel="stylesheet" href="../admin.css">
+</head>
+<body>
+	<?php include '../_components/header.php'; ?>
+	<main>
+
+	</main>
+	<?php include '../../_components/shortFooter.php'; ?>
+</body>
+</html>
