121d5cbc2e56703366de197b42642f94cedb5998
DurgaM <220176950@aston.ac.uk>
2024-03-19 19:28:46 +0000

    added code to connect to database for both test1.php and captcha.php

--- test1.php
+++ test1.php
@@ -30,6 +30,7 @@ if (isset($_POST['input']) && !empty($_POST['input'])) {
 
     <h2>PROVE THAT YOU ARE NOT A ROBOT!!</h2>
     <img src="mirror\captcha.php">
+    mirror\captcha.php
 
     <form method="POST" >
         <input type="text" name="input" />
