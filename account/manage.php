b56ba584ae4ed02f8f5e2e41aba8dcc95a2c0fe5
Pear <pkedzia88@gmail.com>
2024-03-19 14:26:36 +0000

    Re-added details-change to manage.php

--- account/manage.php
+++ account/manage.php
@@ -1,34 +1,111 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) session_start();
 //Send the user back to the index page if they try to access the account manage page without being signed in 
-//if (!isset($_SESSION["userID"])) echo '<script>window.location.replace("../index.php");</script>';
+if (!isset($_SESSION["userID"])) echo '<script>window.location.replace("/login.php");</script>';
 require '../_components/database.php';
-if(isset($_GET['option']) && in_array($_GET['option'], array('details', 'details-change', 'security', 'pastOrders', 'statistics', 'dangerZone', 'link-gooogle', '2fa', 'change-password', 'request-data', 'delete-account'))) {
+$db = new Database();
+
+if (isset($_GET['option']) && in_array($_GET['option'], array('details', 'details-change', 'security', 'pastOrders', 'statistics', 'dangerZone', 'link-gooogle', '2fa', 'change-password', 'request-data', 'delete-account'))) {
     $currentView = $_GET['option'];
 } else $currentView = "details";
+
+// If POST, update database
+$inputError = "";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (!isset($_POST['for'])) {
+        echo "Invalid request";
+        die;
+    }
+
+    switch ($_POST['for']) {
+        case "details-change":
+            $firstName = $_POST['firstName'] ?? null;
+            $surName = $_POST['surName'] ?? null;
+            $email = $_POST['email'] ?? null;
+            $userID = $_SESSION["userID"];
+
+            if ($firstName == null && $surName == null && $email == null) {
+                $inputError = "You must fill in at least one field to change";
+				$currentView = "details-change";
+                break;
+            }
+
+            if (!is_null($firstName) && $firstName != "") {
+                if (strlen($firstName) < 2 || strlen($firstName) > 50) {
+                    $inputError = "First name must be between 2 and 50 characters";
+					$currentView = "details-change";
+                    break;
+                }
+				if (!preg_match("/^[a-zA-Z-' ]*$/", $firstName)) {
+					$inputError .= "First name can only contain letters and spaces.\n";
+					$currentView = "details-change";
+                    break;
+				}
+
+                $sql = "UPDATE users SET firstName = ? WHERE id = ?";
+                $conn = Connection::getConnection();
+                $stmt = $conn->prepare($sql);
+                $stmt->execute([$firstName, $userID]);
+            }
+
+            if (!is_null($surName) && $surName != "") {
+                if (strlen($surName) < 2 || strlen($surName) > 50) {
+					$inputError = "Last name must be between 2 and 50 characters";
+					$currentView = "details-change";
+					break;
+				}
+                if (!preg_match("/^[a-zA-Z-' ]*$/", $surName)) {
+                    $inputError .= "Last name can only contain letters and spaces.\n";
+                    $currentView = "details-change";
+                    break;
+				}
+
+                $sql = "UPDATE users SET lastName = ? WHERE id = ?";
+                $conn = Connection::getConnection();
+                $stmt = $conn->prepare($sql);
+                $stmt->execute([$surName, $userID]);
+            }
+
+            if (!is_null($email) && $email != "") {
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+					$inputError = "Invalid email format";
+					$currentView = "details-change";
+					break;
+				}
+                if (strlen($email) > 320) {
+					$inputError = "Email must be less than 320 characters";
+					$currentView = "details-change";
+					break;
+				}
+
+                $sql = "UPDATE users SET email = ? WHERE id = ?";
+                $conn = Connection::getConnection();
+                $stmt = $conn->prepare($sql);
+                $stmt->execute([$email, $userID]);
+            }
+            break;
+    }
+}
 ?>
 <!doctype html>
 <html lang = "en">
 <head>
-<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include '../_components/default.php'; ?>
     <title>Account Management</title>
-    <link rel="stylesheet" href="../_stylesheets/main.css">
     <link rel="stylesheet" href="../_stylesheets/accountManage.css">
 </head>
 <body>
     <?php include '../_components/header.php'; ?>
     <div class = "sidenav">
         <div>
-            <a href = "manage.php?option=details">Your Details</a><br>
-            <a href = "manage.php?option=security">Security</a><br>
-            <a href = "manage.php?option=pastOrders">Past Orders</a><br>
-            <a href = "manage.php?option=statistics">Statistics</a><br>
-            <a href = "manage.php?option=dangerZone">Danger Zone</a><br><br>
+            <a href="manage.php?option=details">Your Details</a><br>
+            <a href="manage.php?option=security">Security</a><br>
+            <a href="manage.php?option=pastOrders">Past Orders</a><br>
+            <a href="manage.php?option=statistics">Statistics</a><br>
+            <a href="manage.php?option=dangerZone">Danger Zone</a><br><br>
         </div>
     </div>
-    <main class = "main">
+    <main class="main">
         <?php
         switch($currentView) {
             case "details":
@@ -53,6 +130,24 @@ if(isset($_GET['option']) && in_array($_GET['option'], array('details', 'details
                 <?php
                 break;
             case "details-change":
+                ?>
+                <form method="POST" action="">
+                    <input type="hidden" name="for" value="details-change">
+                    <p>Leave fields you wish not to change blank.</p>
+                    <p><?= nl2br($inputError); ?></p>
+                    <br>
+                    <label for="firstName">First Name</label>
+                    <input type="text" name="firstName" id="firstName">
+                    <br>
+                    <label for="surName">Last Name</label>
+                    <input type="text" name="surName" id="surName">
+                    <br>
+                    <label for="email">Email</label>
+                    <input type="text" name = "email" id="email">
+                    <br><br>
+                    <input class="button" type="submit" value="Submit Changes">
+                </form>
+                <?php
                 break;
             case "security":
                 ?>
