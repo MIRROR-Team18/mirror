6676cd00808409bfcfd3dc51bdfa2f2333241b4a
DurgaM <220176950@aston.ac.uk>
2024-03-19 19:00:40 +0000

    made test1.php and added fontfile

---
+++ test1.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+$msg = '';
+
+// If user has given a captcha
+if (isset($_POST['input']) && !empty($_POST['input'])) {
+    // If the captcha is valid
+    if ($_POST['input'] == $_SESSION['captcha']) {
+        $msg = '<span style="color:green">SUCCESSFUL!!!</span>';
+    } else {
+        $msg = '<span style="color:red">CAPTCHA FAILED!!!</span>';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="_stylesheets/main.css"/>
+    <link rel="stylesheet" type="text/css" href="_stylesheets/login.css"/>
+
+    <title> Verify by Captcha </title>
+</head>
+<body>
+<?php include '_components/header.php'; ?>
+
+    <h2>PROVE THAT YOU ARE NOT A ROBOT!!</h2>
+
+    <form method="POST" >
+    <img src="mirror\captcha.php">
+        <input type="text" name="input" />
+        <input type="hidden" name="flag" value="1" />
+        <input type="submit" value="Submit" name="submit" />
+    </form>
+
+    <div style='margin-bottom:5px'>
+        <?php echo $msg; ?>
+    </div>
+
+    <div>
+        Can't read the image? Click
+        <a href='<?php echo $_SERVER['PHP_SELF']; ?>'>
+            here
+        </a>
+        to refresh!
+    </div>
+</body>
+<?php include '_components/footer.php'; ?>
+</html>
