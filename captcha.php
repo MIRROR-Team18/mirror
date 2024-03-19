20a62c4e3b798d1e737cd780af73ec09447db7bd
DurgaM <220176950@aston.ac.uk>
2024-03-19 20:11:20 +0000

    edited comments made in captcha

--- captcha.php
+++ captcha.php
@@ -1,45 +1,38 @@
 <?php
 
-// We start a session to access
-// the captcha externally!
 session_start();
 
  // connect to the database
-  //require_once('./_components/database.php');
-  //$db = new Database();
+  require_once('./_components/database.php');
+  $db = new Database();
 
-// Generate a random number
-// from 1000-9999
+// Generate a random number from 1000-9999
 $captcha = rand(10000, 99999);
 
-// The captcha will be stored
-// for the session
+// The captcha will be stored for the session
 $_SESSION["captcha"] = $captcha; 
 
 // Generate a 90x64 standard captcha image
 $image = imagecreatetruecolor(90, 64); 
 
-// White color
+// White color for the image text
 $white = imagecolorallocate($image, 255, 255, 255);
 
-// Give the image a blue background
+// Give the image a black background
 imagefill($image, 0, 0, 0); 
 
-// Print the captcha text in the image
-// with random position & size
-// Adjusted font size and position for better visibility
-imagestring($image, 5, 10, 20, $captcha, $white);
+//imagestring will print the captcha text within the image, with a position and size.
+imagestring($image, 20, 30, 40, $captcha, $white);
 
-// VERY IMPORTANT: Prevent any Browser Cache!!
+// used to prevent any Browser Cache!!
 header("Cache-Control: no-store, no-cache, must-revalidate"); 
 
-// The PHP-file will be rendered as image
+// makes the PHP-file rendered as an image
 header('Content-type: image/png');
 
-// Finally output the captcha as
-// PNG image the browser
+//the captcha will be ouput as a PNG
 imagepng($image); 
 
-// Free memory
+// destroys the image
 imagedestroy($image);
 ?>
