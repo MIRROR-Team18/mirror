0db3539d21f0a46ee19eff7e4f65588c7a35bd89
PearOrchards <pkedzia88@gmail.com>
2024-03-22 15:11:54 +0000

    Implemented redesign

--- reviews.php
+++ reviews.php
@@ -30,18 +30,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="_stylesheets/main.css">
+    <?php include "_components/default.php"; ?>
     <link rel="stylesheet" href="_stylesheets/reviews.css">
     <title>Rating & Reviews</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap">
     <script src="/_scripts/reviews.js" defer async></script>
 </head>
 <body>
     <?php include "_components/header.php"; ?>
     <div class="header">
-        <h1>Rating & Reviews</h1>
+        <h1>
+            <i class="fa fa-star"></i>
+            REVIEWS
+        </h1>
     </div>
 
     <main class="container">
@@ -49,11 +49,11 @@
             <label for="sort-select">Sort By:</label>
             <form method="POST" action="">
                 <select id="sort-select" name="ordered" onchange="this.form.submit()">
-                    <option value="overall">Overall Rating</option>
-                    <option value="lowest">Lowest Rating</option>
-                    <option value="highest">Highest Rating</option>
-                    <option value="newest">Newest Review</option>
-                    <option value="oldest">Oldest Review</option>
+                    <option value="overall" <?= isset($_POST['ordered']) && $_POST['ordered'] == "overall" ? "selected" : ""; ?>>Overall Rating</option>
+                    <option value="lowest" <?= isset($_POST['ordered']) && $_POST['ordered'] == "lowest" ? "selected" : ""; ?>>Lowest Rating</option>
+                    <option value="highest" <?= isset($_POST['ordered']) && $_POST['ordered'] == "highest" ? "selected" : ""; ?>>Highest Rating</option>
+                    <option value="newest" <?= isset($_POST['ordered']) && $_POST['ordered'] == "newest" ? "selected" : ""; ?>>Newest Review</option>
+                    <option value="oldest" <?= isset($_POST['ordered']) && $_POST['ordered'] == "oldest" ? "selected" : ""; ?>>Oldest Review</option>
                 </select>
             </form>
         </div>
@@ -71,17 +71,19 @@
                     $name = $review["name"];
                     $rating = $review["rating"];
                     $comment = $review["comment"];
-                    $date = $review["date"];
+                    $date = date_format(date_create($review["date"]), "d/m/Y");
                     $imagePath = "/_images/reviews/" . $review["filename"];
+
+					$stars = str_repeat("<i class='fa-solid fa-star'></i>", $rating);
+					$stars = $stars . str_repeat("<i class='fa-regular fa-star'></i>", 5 - $rating);
                 
                     echo <<<HTML
                         <div class="review">
                             <img src="$imagePath" alt="$name's photo" class="review-image">
                             <div class="review-details">
-                                <h3>$name</h3>
-                                <p class="rating">Rating: $rating/5</p>
-                                <p class="comment">Comment: $comment</p>
-                                <p class="date">Date: $date</p>
+                                <h2>$name $stars</h2>
+                                <i class="date">$date</i>
+                                <p class="comment">$comment</p>
                             </div>
                         </div>
                     HTML;
